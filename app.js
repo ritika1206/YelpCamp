b4fe8a442e5405de1de00fd11088beba33a70dfc
ritika221b <ritikaverma9319@gmail.com>
2020-04-16 00:28:17 +0530

    furthur changes added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -11,6 +13,14 @@ app.get("/", (req, res) => {
 app.get("/campGrounds", (req, res) => {
     // res.send("this will be the landing page of yelpcamp app");
     res.render("campGrounds");
+});
+
+app.post("/campGrounds", (req, res) => {
+    res.send("this is the campground post route");
+});
+
+app.get("/campGround/new", (req, res) => {
+    res.render("new");
 });
 
 var server = app.listen('3636', () => {
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Initial Routs
+     -add landing page
+     -add campgroung page that lists all campgrounds
+        each campground has
+            -name
+            -image
+
+# layout
+    -partials added
+    -bootstrap 3.3.5 added
+
+# 
+    
