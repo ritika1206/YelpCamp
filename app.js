66787e2fb99a5e07cf0a804df851e6525bd6036e
ritika221b <ritikaverma9319@gmail.com>
2020-04-17 13:33:40 +0530

    further changes

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
 var express = require("express");
-var ejs = require("ejs");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp-camp-app");
+mongoose.connect(uri, { useNewUrlParser: true });
+mongoose.connect(uri, { useFindAndModify: false });
+mongoose.connect(uri, { useCreateIndex: true });
+
+var campGroundsSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var campGrounds = mongoose.model("campGrounds", "campGroundsSchema");
+
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campGrounds = [
-    {name: "majestic Hue", image: "https://im.rediff.com/getahead/2016/may/12camping1.jpg" },
-    {name: "solace mountain", image: "https://assets.traveltriangle.com/blog/wp-content/uploads/2016/10/Camping-Sites-Near-Bangalore.jpg"},
-    {name: "panaromic view", image: "https://www.arabnews.com/sites/default/files/styles/n_670_395/public/main-image/2018/11/26/1380296-1891588228.jpg?itok=rD_hHuxx"}
-]
-
 app.get("/", (req, res) => {
-    // res.send("this will be the landing page of yelpcamp app");
     res.render("landing");
 });
 
 app.get("/campGrounds", (req, res) => {
-    // res.send("this will be the landing page of yelpcamp app");
     res.render("campGrounds", {campGrounds: campGrounds});
 });
 
-app.post("/campGrounds", (req, res) => {
-    // res.send("<h1>this is the campground post route<h1>");
+app.post("/campGrounds", (req, res) => {    
     var name = req.body.name;
     var image = req.body.image;
-    var campground = {name: name, image: image};
-    campGrounds.push(campground);
+    
+    campGrounds.create({name: name, image: image});
+
     res.redirect("/campGrounds");
 });
 
